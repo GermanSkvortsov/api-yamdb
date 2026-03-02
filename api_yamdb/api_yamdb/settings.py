51dec26f22841493a3406746697a3e4b5fdd9a63
GermanSkvortsov <Geerman1992@gmail.com>
2026-03-02 21:10:51 +0300

    Создал и зарегистрировал приложения users, titles, reviews, api и и настроил кастомную модель User.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'titles',
+    'reviews',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -101,3 +105,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+AUTH_USER_MODEL = 'users.User'
