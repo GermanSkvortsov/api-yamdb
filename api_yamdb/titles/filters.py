ffb2027485cf4c2dcec0cfc240df27fcf371316d
Roman Nechai <rom4n.nechai@yandex.ru>
2026-03-05 01:14:33 +0300

    Добавил загрузку данных из csv файлов

--- api_yamdb/titles/filters.py
+++ api_yamdb/titles/filters.py
@@ -7,7 +7,10 @@ class TitleFilter(django_filters.FilterSet):
     category = django_filters.CharFilter(field_name='category__slug')
     genre = django_filters.CharFilter(field_name='genre__slug')
     year = django_filters.NumberFilter(field_name='year')
-    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    name = django_filters.CharFilter(
+        field_name='name',
+        lookup_expr='icontains'
+    )
 
     class Meta:
         model = Title
