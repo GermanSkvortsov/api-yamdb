312c6a41b9e5bbe9074bdd3e06d102c22a2f7812
Roman Nechai <rom4n.nechai@yandex.ru>
2026-03-05 23:11:17 +0300

    Доработал возвращаеные данные (не просто слаги, а ссылки)

--- api_yamdb/titles/views.py
+++ api_yamdb/titles/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, viewsets, mixins
-
+from rest_framework import filters, viewsets, mixins, status
+from rest_framework.response import Response
 
 from .serializers import (
     CategorySerializer,
@@ -44,8 +44,12 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.annotate(
         rating=Avg('reviews__score', default=None)
     ).order_by('id')
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,
+                       filters.SearchFilter,
+                       filters.OrderingFilter)
     filterset_class = TitleFilter
+    search_fields = ('name', 'description')
+    ordering_fields = ('name', 'year', 'rating')
     permission_classes = (IsAdminOrReadOnly,)
     http_method_names = ['get', 'post', 'patch', 'delete']
 
@@ -53,3 +57,30 @@ class TitleViewSet(viewsets.ModelViewSet):
         if self.action in ('list', 'retrieve'):
             return TitleSerializer
         return TitleCreateSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+        instance.rating = None
+        response_serializer = TitleSerializer(instance, context={'request': request})
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=partial
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        instance = Title.objects.annotate(
+            rating=Avg('reviews__score', default=None)
+        ).get(pk=instance.pk)
+        response_serializer = TitleSerializer(
+            instance,
+            context={'request': request}
+        )
+        return Response(response_serializer.data, status=status.HTTP_200_OK)
